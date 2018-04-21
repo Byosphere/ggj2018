980f40c57d223ddc1f675fef902903ed58e18a29
campignon <camille.dourferpignon@gmail.com>
2018-04-21 17:23:57 +0200

    Ajout page d'accueil avec bouton "Jouer"

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/game', function(req, res) {
+    res.sendFile(__dirname + '/public/game.html');
+});
+
 app.get('/instructions', function (req, res) {
     res.sendFile(__dirname + '/public/howtoplay.html');
 });
